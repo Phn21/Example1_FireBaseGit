873e746b6fd7b7133050c450b3a76422b2880250
unknown <aleksandre.soselia.1@btu.edu.ge>
2022-10-18 16:12:38 +0400

    Initial commit (ExampleTryOne)

--- app/src/main/java/com/example/example1firebasegit/MainActivity.java
+++ app/src/main/java/com/example/example1firebasegit/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< HEAD
+=======
+
+        getSupportActionBar().hide();
+>>>>>>> 36ddbd4 (Initial commit (ExampleTryOne))
     }
 }
