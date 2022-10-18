1eed37fcfe50bacae61320e3e8bbad86173d5c78
L <soselia.aleksandre@gmail.com>
2022-10-18 16:40:17 +0400

    Initial commit (ExampleTryOne)

--- app/src/main/java/com/example/example1firebasegit/MainActivity.java
+++ app/src/main/java/com/example/example1firebasegit/MainActivity.java
@@ -1,10 +1,28 @@
 package com.example.example1firebasegit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +31,49 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+         db = FirebaseFirestore.getInstance();
+         textView = findViewById(R.id.textView);
+
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ako");
+        user.put("last", "Blue");
+        user.put("born", 1815);
+
+
+        // Create a new user with a first, middle, and last name
+        Map<String, Object> userm = new HashMap<>();
+        userm.put("first", "Mary");
+        userm.put("middle", "Red");
+        userm.put("last", "Turing");
+        userm.put("born", 1912);
+
+
+        // Add a new document with a generated ID
+        db.collection("userA").add(user);
+
+
+    }
+
+    public void go(View view) {
+
+        db.collection("userA")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                textView.setText(document.getData() + "\n");
+
+                                Log.d("TAG", document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w("TAG", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 }
