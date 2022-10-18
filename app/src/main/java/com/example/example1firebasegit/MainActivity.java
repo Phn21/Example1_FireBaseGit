c3a05fa51e5f629f1962d80357e30e7a2a9f8451
L <soselia.aleksandre@gmail.com>
2022-10-18 16:17:28 +0400

    Initial commit (ExampleTryOne)

--- app/src/main/java/com/example/example1firebasegit/MainActivity.java
+++ app/src/main/java/com/example/example1firebasegit/MainActivity.java
@@ -10,10 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-=======
 
         getSupportActionBar().hide();
->>>>>>> 36ddbd4 (Initial commit (ExampleTryOne))
+
     }
 }
